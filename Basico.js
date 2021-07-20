69bebb88b2fa07935132a668096a1cfc58eef714
13U1U <79541818+13U1U@users.noreply.github.com>
2021-07-20 18:13:19 -0300

    Create Basico.js

---
+++ Basico.js
@@ -0,0 +1,24 @@
+// VARIAVEIS
+
+// constantes não podem ser mudadas 
+const idade = 21;
+const nome = "Mattheus";
+const sobrenome = "Russi";
+
+// let pode ser alterada para qualquer tipo de variavel
+let ano = 2021;
+
+
+// ESCREVENDO NO CONSOLE
+console.log(nome+ " " +sobrenome);
+
+
+// MATEMATICA
+console.log(10 + 4 * 3);
+console.log((10 + 4) * 3);
+
+console.log("ano " + 2020);
+console.log("2" + "2");
+
+// convertendo tipos
+console.log(parseInt("2") + parseInt("2"));
