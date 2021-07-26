479702ec98bfc5ee3c277acf43bb96dce3cbfa03
13U1U <79541818+13U1U@users.noreply.github.com>
2021-07-26 11:43:27 -0300

    Update 02-OABjavascript.js

--- 02-OABjavascript.js
+++ 02-OABjavascript.js
@@ -1,1 +1,14 @@
+// CLASE BASE
+class Cliente{
+    nome;
+    cpf;
+    agencia;
+}
 
+const cliente1 = new Cliente();
+
+cliente1.nome = 'pedro';
+cliente1.cpf = 08812267689;
+cliente1.agencia = 1001;
+
+console.log(cliente1);
