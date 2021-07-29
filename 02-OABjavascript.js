eb734ecb42851e09f059ea55f0c4e43e91546045
13U1U <79541818+13U1U@users.noreply.github.com>
2021-07-29 09:52:29 -0300

    Update 02-OABjavascript.js

--- 02-OABjavascript.js
+++ 02-OABjavascript.js
@@ -12,3 +12,15 @@ cliente1.cpf = 08812267689;
 cliente1.agencia = 1001;
 
 console.log(cliente1);
+
+// definindo funções
+class ContaCorente{
+    agencia;
+    saldo;
+
+    sacar(valor){
+        if (this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    }
+}
